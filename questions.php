2e4265b2692b2bfb1d4ef9739bb4d5804abceeca
urvish2930 <110124248+urvish2930@users.noreply.github.com>
2025-01-12 21:05:21 +0530

    Add files via upload

---
+++ questions.php
@@ -0,0 +1,31 @@
+<?php
+$questions = [         //This is an question array of the quiz
+    [
+        'text' => 'What is the captain of indian cricket team?',
+        'options' => ['Rohit', 'virat', 'Dhoni', 'Rahul'],
+        'answer' => 0 // Correct answer
+    ],
+    [
+        'text' => 'Which language is used for web development?',
+        'options' => ['Python', 'PHP', 'Java', 'C++'],
+        'answer' => 1
+    ],
+    [
+        'text' => 'What is captial of gujarat?',
+        'options' => ['Ahmedabad', 'Gandhinagar', 'Rajkot', 'Bhavnagar'],
+        'answer' => 1
+    ]
+    ,
+    [
+        'text' => 'Which is the highest run scorer of WC2023?',
+        'options' => ['Rohit', 'Virat', 'Shubhman', 'Rahul'],
+        'answer' => 1
+    ]
+    ,
+    [
+        'text' => 'Who is the cheif minister of gujarat?',
+        'options' => ['Rupani', 'Anandiben', 'Bhupendra', 'Kejriwal'],
+        'answer' => 2
+    ]
+];
+?>
